85407a01ec0827b910e485a4b2ae4a30f815c088
rd <richardpsdsilva@gmail.com>
2026-05-08 11:08:17 -0300

    rota de cadasto

---
+++ main.py
@@ -0,0 +1,32 @@
+#Criar um sistema SSR 
+
+from fastapi import FastAPI , Depends , Request , Form
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+app = FastAPI(title = "Sistema de login simples")
+
+# Rodar esse codigo:
+# python -m uvicorn main:app --reload
+
+templates = Jinja2Templates(directory = "templates")
+
+#Rota - metodo HTTP (get , post)
+
+@app.get("/cadastro")
+def tela_cadastro(request:Request):
+    return templates.TemplateResponse(
+        request,
+        "cadastro.html",
+        {"request": request}
+    )
+
+app.get("/login")
+def tela_login(request: Request):
+     return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"request": request}
+    )
+    
